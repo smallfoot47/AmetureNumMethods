230b59bcfdddb736d92149c3fb717c9f0a67f42a
Vivek Radhakrishnan <55615242+SassyQuatch47@users.noreply.github.com>
2020-08-07 08:58:28 +0530

    Added templated SigmoidInterpolate() in NumMethods.h
    SigmoidInterpolate(foot, head, affinity) converts from (-inf, inf)(affinity) space to [foot, head] space according to the transformed sigmoid curve.
    Renamed Interpolate() to templated LinearInterpolate() and templated it.
    Also cleaned stuff up in NumMethods.h.

--- NumMethods.h
+++ NumMethods.h
@@ -1,9 +1,8 @@
 #pragma once
-#include <functional>
 #include <vector>
-#include <unordered_map>
 
 #include "Containers.h"
+#include "Func++.h"
 
 Coord2D IntermediateValue(std::function<double(double)> f, Interval<double> interval, uint8_t n);//Closest zero within iterative limits
 inline Coord2D NewtonsMethod(std::function<double(double)> f, double x0, uint8_t n);//Approximate Newton's Method for approaching roots
@@ -13,14 +12,17 @@ inline Coord2DMap Function_to_Map(std::function<double(double)> f, Interval<doub
 Coord2DMap Function_to_Map(std::function<double(double)> f, Interval<double> interval, uint32_t n);//converts a bounded function to a Coord2DMap, taking n discrete points from f
 Coord2DMap Function_to_Map(std::function<double(double)> f, Interval<double> interval, double epsilon);//converts a bounded function to a Coord2DMap, seperated by epsilon amoung adjacent points along X
 
-template <class V_t, class P_t>
-V_t Interpolate(V_t foot, V_t head, P_t percent);
-
 std::function<double(double)> Interpolate(Coord2DMap map, Interval<double> interval);//makes mapping linearly continous
 std::function<double(double)> Interpolate(Coord2DMap map, Interval<double> interval, uint8_t poly_factor, uint32_t disloc_factor = 0);//makes mapping linearly continous with vertice-count controlled by poly_factor and input bias controlled by disloc_factor
 
 template<class V_t, class P_t>
-inline V_t Interpolate(V_t foot, V_t head, P_t percent)
+inline V_t LinearInterpolate(V_t foot, V_t head, P_t percent)//converts from [0, 1] space to [foot, head] space linearly
 {
 	return foot + (head - foot) * percent;
+}
+
+template<class V_t, class P_t>
+inline V_t SigmoidInterpolate(V_t foot, V_t head, P_t affinity)//converts from (-inf, inf) space to [foot, head] space wrt the sigmoid function
+{
+	return foot + (head - foot) * sigmoid(affinity);
 }
