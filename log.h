430a5d5fec94d687b137feaa15c997eaaef0cd93
Vivek Radhakrishnan <55615242+SassyQuatch47@users.noreply.github.com>
2020-08-07 08:27:16 +0530

    Added FIXED_LOG() macro to log.h
    FIXED_LOG() fixes the cursor just ahead of the end of the LOG message

--- log.h
+++ log.h
@@ -2,9 +2,11 @@
 #include <iostream>
 //define ENABLE_LOG before inclusion to enable
 #ifdef ENABLE_LOG
-#define LOG(x) std::cout << "LOG::" << x << std::endl //logs statements concatinated by cascades
-#define LOG_COMMAND(x) x //logs statement executions
+ #define LOG(x) std::cout << "LINE:" << __LINE__ << ":: " << x << std::endl //logs statements concatinated by cascades
+ #define FIXED_LOG(x) std::cout <<"LINE:" << __LINE__ << ":: " << x << " "; //logs statements concatinated by cascades and fixes the cursor at the end 
+ #define LOG_COMMAND(x) x //logs statement executions
 #else
-#define LOG(x) //#define ENABLE_LOG before #include "...log.h" to enable
-#define LOG_COMMAND(x) //#define ENABLE_LOG before #include "...log.h" to enable
+ #define LOG(x) //#define ENABLE_LOG before #include "...log.h" to enable
+ #define FIXED_LOG(x) //#define ENABLE_LOG before #include "...log.h" to enable
+ #define LOG_COMMAND(x) //#define ENABLE_LOG before #include "...log.h" to enable
 #endif
