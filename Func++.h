ca0e2e035e708dd08cb8d530a5e3d162f96427fd
Vivek Radhakrishnan <55615242+SassyQuatch47@users.noreply.github.com>
2020-08-07 08:52:44 +0530

    Added templated sigmoid() function in Func++.h
    Added Mathematical sigmoid template function sigmoid()

--- Func++.h
+++ Func++.h
@@ -3,3 +3,21 @@
 #include <limits>
 
 std::function<double(double)> derive(std::function<double(double)>& f); //Numerical approxiamation of derivative
+/*
+template <class N_t>
+inline std::function<N_t(N_t)> derive(std::function<N_t(N_t)>& f)
+{
+	N_t dx = std::numeric_limits<N_t>::epsilon();
+	N_t denom = 2 * dx;
+
+	return [&](N_t x)
+	{
+		return (f(x + dx) - f(x - dx)) / denom;
+	};
+}
+*/
+template <class N_t>
+inline N_t sigmoid(N_t x)//Mathematical sigmoid function
+{
+	return 1 / (exp(-x) + 1);
+}
