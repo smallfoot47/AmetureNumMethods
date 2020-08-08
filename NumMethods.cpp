39ac6340d40324deb3352df21c1f836120d089a5
Vivek Radhakrishnan <55615242+SassyQuatch47@users.noreply.github.com>
2020-08-08 14:47:02 +0530

    Updated NumMethods.cpp
    Modified the logic of Function_to_Map() and implemented a new overload of Interpolate() taking in node count.
    Also cleared up lots of unnecessary implicit typecasting.

--- NumMethods.cpp
+++ NumMethods.cpp
@@ -5,12 +5,12 @@ Coord2D BisectionMethod(std::function<double(double)> f, Interval<double> interv
 	double x = interval.mean();
 	double y = f(x);
 
-	if (n > 1)
+	if (n > 1u)
 	{
-		if (y * f(interval.lower) < 0)
-			return BisectionMethod(f, { interval.lower, x }, n - 1);
-		else if (y * f(interval.upper) < 0)
-			return BisectionMethod(f, { x, interval.upper }, n - 1);
+		if (y * f(interval.lower) < 0.0)
+			return BisectionMethod(f, { interval.lower, x }, n - 1u);
+		else if (y * f(interval.upper) < 0.0)
+			return BisectionMethod(f, { x, interval.upper }, n - 1u);
 	}
 
 	return { x, y };
@@ -27,15 +27,15 @@ Coord2D NewtonsMethod(std::function<double(double)> f, std::function<double(doub
 	{
 		x0 -= f(x0) / df(x0);
 
-		if (n > 1)
-			return NewtonsMethod(f, df, x0, n - 1);
+		if (n > 1u)
+			return NewtonsMethod(f, df, x0, n - 1u);
 	}
 	return { x0, f(x0) };
 }
 
-inline Coord2DMap Function_to_Map(std::function<double(double)> f, Interval<double> interval)
+Coord2DMap Function_to_Map(std::function<double(double)> f, Interval<double> interval)
 {
-	return Function_to_Map(f, interval, std::numeric_limits<double>::epsilon());
+	return Function_to_Map(f, interval, 1E-5);
 }
 
 Coord2DMap Function_to_Map(std::function<double(double)> f, Interval<double> interval, uint32_t n)
@@ -44,7 +44,7 @@ Coord2DMap Function_to_Map(std::function<double(double)> f, Interval<double> int
 
 	double epsilon = interval / n;
 
-	for (uint32_t i = 0; i <= n; ++i)
+	for (uint32_t i = 0u; i <= n; ++i)
 	{
 		double x = interval.first + i * epsilon;
 		F[x] = f(x);
@@ -55,9 +55,10 @@ Coord2DMap Function_to_Map(std::function<double(double)> f, Interval<double> int
 
 Coord2DMap Function_to_Map(std::function<double(double)> f, Interval<double> interval, double epsilon)
 {
-	Coord2DMap F((size_t)(interval / epsilon + 1));
-
-	for (uint32_t i = 0; i < F.size(); ++i)
+	uint32_t size = (uint32_t)(interval / epsilon) + 1u;
+	Coord2DMap F; F.reserve(size);
+	
+	for (uint32_t i = 0u; i < size; ++i)
 	{
 		double x = interval.first + i * epsilon;
 		F[x] = f(x);
@@ -66,39 +67,44 @@ Coord2DMap Function_to_Map(std::function<double(double)> f, Interval<double> int
 	return F;
 }
 
-std::function<double(double)> Interpolate(Coord2DMap map, Interval<double> interval)
+std::function<double(double)> Interpolate(Coord2DMap& map, const Interval<double>& interval)
 {
-	const double internodal_width = interval / map.size();
+	return Interpolate(map, interval, map.size());
+}
+
+std::function<double(double)> Interpolate(Coord2DMap& map, const Interval<double>& interval, uint32_t n)
+{
+	const double internodal_width = interval / (n - 1u);
 	const double bias = -interval.first;
 
-	return [&](double x)->double
+	return [=](double x) mutable
 	{
 		if (interval[x])
 		{
 			x = (x + bias) / internodal_width;
 			double foot = floor(x) * internodal_width - bias;
-			return LinearInterpolate(map[foot], map[foot + internodal_width], remainder(x, 1));
+			return LinearInterpolate(map[foot], map[foot + internodal_width], abs(remainder(x, 1.0)));
 		}
 
-		return 0;
+		return 0.0;
 	};
 }
 
-std::function<double(double)> Interpolate(Coord2DMap map, Interval<double> interval, uint8_t poly_factor, uint32_t disloc_factor)
+std::function<double(double)> Interpolate(Coord2DMap& map, const Interval<double>& interval, uint16_t poly_factor, uint32_t disloc_factor)
 {
-	double internodal_width = interval / map.size();
+	double internodal_width = interval / (map.size() - 1u);
 	double bias = disloc_factor * internodal_width - interval.first;
 	internodal_width *= poly_factor;
 
-	return [&](double x)->double
+	return [=](double x) mutable
 	{
 		if (interval[x])
 		{
 			x = (x + bias) / internodal_width;
 			double foot = floor(x) * internodal_width + interval.first;
-			return LinearInterpolate(map[foot], map[foot + internodal_width], remainder(x, 1));
+			return LinearInterpolate(map[foot], map[foot + internodal_width], abs(remainder(x, 1.0)));
 		}
 
-		return 0;
+		return 0.0;
 	};
 }
