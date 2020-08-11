086464688e0526a210da1ed04e2804ec2a8e36c7
Vivek Radhakrishnan <55615242+SassyQuatch47@users.noreply.github.com>
2020-08-11 23:11:50 +0530

    Updated FuncShorts.h
    Changed PRINT_AT_VALUE() to PRINT_AT_VAR() and created a new function PRINT_AT_VALUE().

--- FuncShorts.h
+++ FuncShorts.h
@@ -1,11 +1,11 @@
 #pragma once
+#include <functional>
 #include <iostream>
 #include <vector>
 
-#include "NumMethods.h"
-
-#define MAKE_FUNC(def) [&](double x) { def } //eg:- MAKE_FUNCTION( return pow(x, 2) - 1; )
-#define PRINT_AT_VALUE(function, value) std::cout << #function << "(" << #value << ") = " << function(value) << std::endl //eg:- PRINT_AT_VALUE(abs, -2);
+#define MAKE_FUNC(def) [&](double x) { def } //eg:- MAKE_FUNCTION( return pow(x, 2) - 1; );
+#define PRINT_AT_VAR(function, value) std::cout << #function << "(" << #value << ") = " << function(value) << std::endl //eg:- PRINT_AT_VAR(abs, x);
+#define PRINT_AT_VALUE(function, value) std::cout << #function << "(" << value  << ") = " << function(value) << std::endl //eg:- PRINT_AT_VALUE(abs, -2);
 #define PRINT_FUNC_COORD(answer) std::cout << #answer << "(" << answer.first << ") = " << answer.second << std::endl //eg:- PRINT_FUNC_COORD({ 2.5, 1 });
 
 typedef std::function<double(double)> func_t;//double->double function storage object
