559f7fc08d9dc9fb9a889e673f973391cf4c7f3e
Vivek Radhakrishnan <55615242+SassyQuatch47@users.noreply.github.com>
2020-08-11 23:09:51 +0530

    Updated NumMethods.cpp
    Added the implementation of SecantMethod(), added only<> restrictions and clean stuff up a bit.

--- NumMethods.cpp
+++ NumMethods.cpp
@@ -50,6 +50,75 @@ Coord2DMap Function_to_Map(std::function<double(double)> f, Interval<double> int
 		F[x] = f(x);
 	}
 
+#include "NumMethods.h"
+
+Coord2D BisectionMethod(std::function<double(double)> f, Interval<double> interval, uint8_t n)
+{
+	double x = interval.mean();
+	double y = f(x);
+
+	if (n > 1u)
+	{
+		if (y * f(interval.lower) < 0.0)
+			return BisectionMethod(f, { interval.lower, x }, n - 1u);
+		else if (y * f(interval.upper) < 0.0)
+			return BisectionMethod(f, { x, interval.upper }, n - 1u);
+	}
+
+	return { x, y };
+}
+
+inline Coord2D NewtonsMethod(std::function<double(double)> f, only<double> x0, uint8_t n)
+{
+	return NewtonsMethod(f, derive(f), x0, n);
+}
+
+Coord2D NewtonsMethod(std::function<double(double)> f, std::function<double(double)> df, only<double> x0, uint8_t n)
+{
+	if (df(x0))
+	{
+		x0 -= f(x0) / df(x0);
+
+		if (n > 1u)
+			return NewtonsMethod(f, df, x0, n - 1u);
+	}
+	return { x0, f(x0) };
+}
+
+Coord2D SecantMethod(std::function<double(double)> f, only<double> x0, only<double> x1, uint8_t n)
+{
+	double F0 = f(x0), F1 = f(x1);
+
+	if(F1 != F0)
+	{
+		double x2 = x1 - F1 * (x1 - x0) / (F1 - F0);
+		
+		if (n > 2)
+			return SecantMethod(f, x1, x2, n - 1);
+		
+		return { x2, f(x2) };
+	}
+
+	return { x1, F1 };
+}
+
+Coord2DMap Function_to_Map(std::function<double(double)> f, Interval<double> interval)
+{
+	return Function_to_Map(f, interval, 1E-5);
+}
+
+Coord2DMap Function_to_Map(std::function<double(double)> f, Interval<double> interval, uint32_t n)
+{
+	Coord2DMap F(n--);
+
+	double epsilon = interval / n;
+
+	for (uint32_t i = 0u; i <= n; ++i)
+	{
+		double x = interval.first + i * epsilon;
+		F[x] = f(x);
+	}
+
 	return F;
 }
 
