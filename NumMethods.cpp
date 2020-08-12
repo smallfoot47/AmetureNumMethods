3709c2c07a54ceff6f6fbe9eeaf220d62694fbc1
Vivek Radhakrishnan <55615242+SassyQuatch47@users.noreply.github.com>
2020-08-12 08:52:57 +0530

    Updated NumMethods.cpp
    cleaned it up.

--- NumMethods.cpp
+++ NumMethods.cpp
@@ -16,58 +16,6 @@ Coord2D BisectionMethod(std::function<double(double)> f, Interval<double> interv
 	return { x, y };
 }
 
-inline Coord2D NewtonsMethod(std::function<double(double)> f, double x0, uint8_t n)
-{
-	return NewtonsMethod(f, derive(f), x0, n);
-}
-
-Coord2D NewtonsMethod(std::function<double(double)> f, std::function<double(double)> df, double x0, uint8_t n)
-{
-	if (df(x0))
-	{
-		x0 -= f(x0) / df(x0);
-
-		if (n > 1u)
-			return NewtonsMethod(f, df, x0, n - 1u);
-	}
-	return { x0, f(x0) };
-}
-
-Coord2DMap Function_to_Map(std::function<double(double)> f, Interval<double> interval)
-{
-	return Function_to_Map(f, interval, 1E-5);
-}
-
-Coord2DMap Function_to_Map(std::function<double(double)> f, Interval<double> interval, uint32_t n)
-{
-	Coord2DMap F(n--);
-
-	double epsilon = interval / n;
-
-	for (uint32_t i = 0u; i <= n; ++i)
-	{
-		double x = interval.first + i * epsilon;
-		F[x] = f(x);
-	}
-
-#include "NumMethods.h"
-
-Coord2D BisectionMethod(std::function<double(double)> f, Interval<double> interval, uint8_t n)
-{
-	double x = interval.mean();
-	double y = f(x);
-
-	if (n > 1u)
-	{
-		if (y * f(interval.lower) < 0.0)
-			return BisectionMethod(f, { interval.lower, x }, n - 1u);
-		else if (y * f(interval.upper) < 0.0)
-			return BisectionMethod(f, { x, interval.upper }, n - 1u);
-	}
-
-	return { x, y };
-}
-
 inline Coord2D NewtonsMethod(std::function<double(double)> f, only<double> x0, uint8_t n)
 {
 	return NewtonsMethod(f, derive(f), x0, n);
